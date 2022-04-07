22275e543224333a16c842c21f26accf7f00399f
Evgeny Torukas <evgeny@MacBook-Air-Evgeny.local>
2022-04-08 03:34:47 +0400

    some fixed

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,22 +4,21 @@ class App {
         this.eventListeners();
     };
 
-    eventListeners() {
+    async eventListeners() {
         const showID = document.querySelector('.show_idBTN');
         if (showID) {
-            (async _ => {
-                const _fac = chrome.runtime.getURL('../assets/js/background/facInit.js');
-                await import(_fac).then(Fac => {
-                    const fac = new Fac.fac, _style = getComputedStyle(document.body).backgroundColor;      
-                    fac.isDark(_style) ? showID.classList.add('dark') : showID.classList.remove('dark');
-                });
-    
-                const getID = chrome.runtime.getURL('../assets/js/background/requests/getID.js');
-                await import(getID).then(vkID => {
-                    const classvkID = new vkID.getID, id = location.pathname.replace(/[\\\/]/g, '');
-                    showID ? showID.onclick = _ => classvkID.request(id) : false; 
-                });
-            })();
+            const _fac = chrome.runtime.getURL('../assets/js/background/facInit.js'),
+                getID = chrome.runtime.getURL('../assets/js/background/requests/getID.js');
+            
+            await import(_fac).then(Fac => {
+                const fac = new Fac.fac, _style = getComputedStyle(document.body).backgroundColor;      
+                fac.isDark(_style) ? showID.classList.add('dark') : showID.classList.remove('dark');
+            });
+            
+            await import(getID).then(vkID => {
+                const classvkID = new vkID.getID, id = location.pathname.replace(/[\\\/]/g, '');
+                showID ? showID.onclick = _ => classvkID.request(id) : false; 
+            });
         };
     };
 
